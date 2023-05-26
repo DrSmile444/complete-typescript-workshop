231873f0e48ee18f566ad49f3ca74f1bd9c570f0
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-05-26 22:36:21 +0300

    feat: add indexed access types

---
+++ workshop/2-basic-concepts/indexed-access-types.ts
@@ -0,0 +1,24 @@
+// 📎 Reference:
+// Youtube - Matt Pocock - The SECRET POWER of indexed access types - Advanced TypeScript
+// https://www.youtube.com/watch?v=plsnFfbqVEo
+
+// Get one key
+interface Colors {
+    primary: "blue",
+    secondary: "yellow",
+}
+
+type PrimaryType = unknown;
+
+// Get all keys from array
+
+const letters = ['a', 'b', 'c'];
+
+type EveryLetter = unknown;
+
+// Get nested types
+
+const userRoleConfig = {
+    user: ["view"],
+    admin: ["view", "edit", "delete"],
+};
